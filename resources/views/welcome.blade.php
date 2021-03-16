fa8cd5ca67571bbab6663c5d1c9963deb3ec7730
HYH <h2005yh@126.com>
2021-03-16 08:29:15 +0000

    这是第二次提交hyhyh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,7 +36,7 @@
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">Hello Laravel! - by Summer</div>
+        <div class="title">Hello Laravel! - by HYH</div>
     </div>
 </div>
 </body>
